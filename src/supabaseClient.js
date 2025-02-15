271047c103e2abc43acab49fa766a6fe371d4a5a
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2025-02-15 17:47:31 +0530

    Initial commit

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,7 +4,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // Create a single supabase client for interacting with your database
 const supabase = createClient(supabaseUrl, supabaseKey);
-console.log("Hii")
+
 
 export default supabase;
 
